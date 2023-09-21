e67acc815359ae9bafd0aab8d969c30213092c85
Jamila Mahamed <j.mahamed95@icloud.com>
2023-09-21 20:47:25 +0200

    changes

--- src/app/components/Contact/Contact.js
+++ src/app/components/Contact/Contact.js
@@ -6,7 +6,6 @@ function Contact() {
 
             <div className='blur3'></div>
             <div className="blur4"></div>
-            <div className="blur5"></div>
             <div className="contact-card">
                 <h4>Interested in hiring me?!</h4>
                 <a href="mailto:jamila.mahamed95@icloud.com">
